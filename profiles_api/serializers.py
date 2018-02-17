863635e5274912fafd598b1bd393a2aca4bba34c
Tikumporn Wankvar <thesecondstageshow@gmail.com>
2018-02-17 12:36:43 +0700

    Added Login API

---
+++ profiles_api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from . import models
+
+class HelloSerializer(serializers.Serializer):
+    """Serializes a name field for testing out APIView"""
+
+    name = serializers.CharField(max_length=10)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serialzer for user Profile"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ['id','name','email','password']
+        extra_kwargs = {'password' : { 'write_only' : True}}
+
+    def create(self, validated_data):
+        """ Created and return new user"""
+
+        user = models.UserProfile(
+            email = validated_data['email'],
+            name = validated_data['name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
